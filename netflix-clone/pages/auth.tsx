9ec1d822b62a1f497da241c565ce9ebc60395a25
Öykü Kaya <oykuky001@gmail.com>
2024-02-26 16:52:45 +0300

    input component added

--- netflix-clone/pages/auth.tsx
+++ netflix-clone/pages/auth.tsx
@@ -1,6 +1,10 @@
-import Input from "@/components/input";
+import Input from "@/components/Input";
+import { useState } from "react";
 
 const Auth = () => {
+    const [email,setEmail] = useState (''); 
+    const [name,setName] = useState (''); 
+    const [password,setPassword] = useState (''); 
     return  (
         <div className = "relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-center bg-cover">
              <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -9,12 +13,14 @@ const Auth = () => {
                 </nav>
                 <div className="flex justify-center ">
                     <div className="bg-black bg-opacity-70 px-16 py-16 lg:w-2/5 self-center mt-2 lg:max-w-md rounded-md w-full">
-                    <h2 className="text-white text-4xl mb-8 font-semibold">
-                        Sign in
-                    </h2>
-                    <div className="flex flex-col gap-4">
-                        <Input/>        
-                     </div>
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            Sign in
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            <Input label="Username" onChange={(ev : any) => setName(ev.target.value)} id="name" type="text" value={name} />        
+                            <Input label="Email" onChange={(ev : any) => setEmail(ev.target.value)} id="email" type="email" value={email} />        
+                            <Input label="Password" onChange={(ev : any) => setPassword(ev.target.value)} id="password" type="password" value={password} />        
+                        </div>
                     </div>
                 </div>
              </div>
