a7025c4af97ba50365bb2a88ee53d3379390b6d5
Öykü Kaya <oykuky001@gmail.com>
2024-03-06 21:29:55 +0300

    name updated

--- netflix-clone/pages/profiles.tsx
+++ netflix-clone/pages/profiles.tsx
@@ -1,5 +1,7 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -20,14 +22,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 
-
 const Profiles = () => {
+    const {data:user} = useCurrentUser();
+    const router = useRouter();
     return (
         <div className="flex items-center h-full justify-center">
             <div className="flex flex-col">
                 <h1 className="text-3xl md:text-6xl text-white text-center">Who is watching?</h1>
                 <div className="flex items-center justify-center gap-8 mt-10">
-                    <div onClick={()=>{}}>
+                    <div onClick={()=> router.push('/') }>
                         <div className="group flex-row w-44 mx-auto">
                             <div
                             className="
@@ -47,7 +50,7 @@ const Profiles = () => {
                             </div>
 
                             <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
-                                Name
+                                {user?.name}
                             </div>
                         </div>
                     </div>
