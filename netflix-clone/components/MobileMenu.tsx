bf4e97e26b33cdeb4a685fbe0a627bcedd15e7b2
Öykü Kaya <oykuky001@gmail.com>
2024-03-07 15:01:01 +0300

    new profiles component added

--- netflix-clone/components/MobileMenu.tsx
+++ netflix-clone/components/MobileMenu.tsx
@@ -5,7 +5,33 @@ interface MobileMenuProps {
 }
 
 const MobileMenu:React.FC<MobileMenuProps> = ({visible}) => {
-    return (
-        
-   )
+    if(!visible){
+        return null;
+    }
+    return(
+        <div className='bg-black w-56 absolute top-8 left-0 py-5 flex flex-col border-2 border-gray-800 '>
+            <div className='flex flex-col gap-4' >
+                <div className='px-3 text-center text-white hover:underline'>
+                    Home
+                </div>
+                <div className='px-3 text-center text-white hover:underline'>
+                    Series
+                </div>
+                <div className='px-3 text-center text-white hover:underline'>
+                    Films
+                </div>
+                <div className='px-3 text-center text-white hover:underline'>
+                    New & Popular
+                </div>
+                <div className='px-3 text-center text-white hover:underline'>
+                    My List
+                </div>
+                <div className='px-3 text-center text-white hover:underline'>
+                    Browse by languages
+                </div>
+            </div>
+        </div>
+    )
 } 
+
+export default MobileMenu; 
