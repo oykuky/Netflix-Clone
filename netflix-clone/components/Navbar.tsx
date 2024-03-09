71bdeecebcbeb527f1db8162d30f41429a3e7220
Öykü Kaya <oykuky001@gmail.com>
2024-03-09 22:17:56 +0300

    updated

--- netflix-clone/components/Navbar.tsx
+++ netflix-clone/components/Navbar.tsx
@@ -1,10 +1,42 @@
+import { useCallback, useEffect, useState } from "react";
+import MobileMenu from "./MobileMenu";
 import NavbarItem from "./NavbarItem";
 import { BiChevronDown } from "react-icons/bi";
+import { BsSearch, BsBell } from "react-icons/bs";
+import AccountMenu from "./AccountMenu";
+
+const TOP_OFFSET= 66;
 
 const Navbar = () => {
+    const [showMobileMenu,setShowMobileMenu] = useState(false);
+    const [showAccountMenu,setShowAccountMenu] = useState(false);
+    const [showBackground,setShowBackground] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY >= TOP_OFFSET){
+                setShowBackground(true)
+            } else{
+                setShowBackground(false)
+            }
+        } 
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    },[])
+
+    const toggleMobileMenu = useCallback(() =>{
+        setShowMobileMenu((current) => !current );
+    },[] );
+    const toggleAcccountMenu = useCallback(() =>{
+        setShowAccountMenu((current) => !current );
+    },[] );
+
     return (
         <nav className="w-full fixed z-40 ">
-            <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90 ">
+            <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90': ''}`}>
                 <img src="/images/logo.png" className="h-4 lg:h-7" alt="logo" />
                 <div className="flex-row ml-8 gap-7 hidden lg:flex">
                     <NavbarItem label="Home" />
@@ -14,10 +46,25 @@ const Navbar = () => {
                     <NavbarItem label="My List" />
                     <NavbarItem label="Browse by languages" />
                 </div>
-                <div className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+                <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
                     <p className="text-white text-sm">Browse</p>
-                    <BiChevronDown className="text-white transition" />
-                    <MobileMenu/>
+                    <BiChevronDown className={`text-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0'}`} />
+                    <MobileMenu visible = {showMobileMenu} />
+                </div>
+                <div className="flex flex-row ml-auto gap-7 items-center">
+                    <div className="text-gray-200 hover:text-gray-400 cursor-pointer transition">
+                      <BsSearch />
+                    </div>
+                    <div className="text-gray-200 hover:text-gray-400 cursor-pointer transition">
+                      <BsBell />
+                    </div>
+                    <div  onClick={toggleAcccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
+                        <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+                            <img src="/images/default-red.png" alt="" />
+                        </div>
+                        <BiChevronDown  className={`text-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`} />
+                        <AccountMenu visible = {showAccountMenu} />
+                    </div>
                 </div>
             </div>
         </nav>
