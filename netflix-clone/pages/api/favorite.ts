494ff7ccfb02d9bb7019a5de37770a12035969a9
Öykü Kaya <oykuky001@gmail.com>
2024-03-11 23:19:11 +0300

    added

--- netflix-clone/pages/api/favorite.ts
+++ netflix-clone/pages/api/favorite.ts
@@ -32,7 +32,7 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse){
             });
 
             return res.status(200).json(user);
-    }
+        }
 
     if(req.method === 'DELETE'){
         const {currentUser} = await serverAuth(req);
@@ -65,7 +65,7 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse){
     return res.status(405).end();
     }catch(error){
         console.log(error);
-        return res.status(400).end();
+        return res.status(500).end();
     }
 
 }
